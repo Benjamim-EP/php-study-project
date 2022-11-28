e46af8c0ab50ce4dd9d3221c5eafea26adce2300
Benjamim-EP <bannk18@gmail.com>
2022-11-28 10:38:34 -0300

    Obtendo rotas dinamicas

--- php-project/app/router/router.php
+++ php-project/app/router/router.php
@@ -6,9 +6,21 @@ function routes() // onde vai o array de todas as rotas do sistema
 
 function exactMatchUriInArrayRoutes($uri, $routes){
     if(array_key_exists($uri,$routes)){
-        return [];
+        return [$uri => $routes[$uri]];
     }
     return [];
+}
+
+function regularExpressionMatchArrayRoutes($uri, $routes)
+{
+    return array_filter(
+        $routes,
+        function($value) use($uri){
+            $regex = str_replace('/','\/', ltrim($value,'/'));
+            return preg_match("/^$regex$/",ltrim($uri,'/'));
+        },
+        ARRAY_FILTER_USE_KEY
+    );
 }
 
 function router()
@@ -16,6 +28,13 @@ function router()
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $routes = routes();
 
-    $matchedUri = exactMatchUriInArrayRoutes($uri, $routes);
+    //$matchedUri = exactMatchUriInArrayRoutes($uri, $routes);
+
+    if (empty($matchedUri)){
+        $matchedUri = regularExpressionMatchArrayRoutes($uri, $routes);
+    }
+
+    var_dump($matchedUri);
+    die();
 }
 ?>
