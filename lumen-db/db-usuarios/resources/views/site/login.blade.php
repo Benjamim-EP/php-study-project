2476aff12ce0dd6fe5b29210bdc191de6e41b67b
Benjamim-EP <bannk18@gmail.com>
2022-12-01 15:06:18 -0300

    formulário login

---
+++ lumen-db/db-usuarios/resources/views/site/login.blade.php
@@ -0,0 +1,12 @@
+
+@extends('site.master')
+@section('content')
+    <h2>Login</h2>
+
+    <form action="/login" method="post">
+        <input type="text" name="email" value="test@gmail.com">
+        <input type="text" name="password" value="123">
+        <button type="submit">Login</button>
+    </form>
+
+@stop
