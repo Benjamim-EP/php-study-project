6b7fffbc2f129a7af4600facd5a7fa1c21a00edd
Benjamim-EP <bannk18@gmail.com>
2022-12-01 10:28:48 -0300

    Mostrar posts do BD

--- lumen-db/db-usuarios/app/Http/Controllers/Home.php
+++ lumen-db/db-usuarios/app/Http/Controllers/Home.php
@@ -1,11 +1,14 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 
 class Home extends Controller
 {
     public function index()
     {
+        $posts = Post::all();
+        echo $posts;
         return view('site.home',['title' => 'Home', 'name' => 'Benjamim']);
     }
 }
